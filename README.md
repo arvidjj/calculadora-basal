19f4606c5edce2ad27489f5b532cca3159e66fe2
arvidjj <andy.oj14@gmail.com>
2023-09-06 16:13:03 -0400

    Primer commit, esqueleto html, sin funcionalidad

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calculadora Basal
+
+Hecho para el pre-entrenamiento de Programando Paraguay
+
+Para ejecutar
+```
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className='two-card'>
+        <div className='card card1'>
+          <h1>Hidratación Basal</h1>
+          <p className='minititulo'>Completa todos los datos</p>
+
+          <input className='inputcustom' type="text" name="peso" id="peso" placeholder='Ingresa el peso'/>
+          <button className='mainbutton'>Calcular</button>
+
+          <div style={{"margin-top":"5px"}}>
+            <p className='infotext'>
+              70 cc/hr
+            </p>
+            <p className='infotext'>
+              m+m/2 105 cc cc/hr
+            </p>
+          </div>
+        </div>
+        <div className='card card2'>
+          <h1>¿Como se calcula?</h1>
+
+          <ul>
+            <li className='listitem'>
+                De 0kg a 10kg, se calcular 100cc por cada kilo.
+            </li>
+            <li>
+              Se suman 50cc por cada kilo de peso por arriba de 10kg hasta 20 kg
+            </li>
+            <li>
+              De 20kg para arriba, se suman 20cc por cada kilo adicional
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
